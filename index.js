0d2c49b6595f64a961f1b5f9d2fd48eedece4178
Ken <tushar260104@gmail.com>
2026-03-30 01:34:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const str = 'Tushar'
+const len = str.length
+const upper = str.toUpperCase()
+const lower = str.toLowerCase()
+console.log(str)
+console.log(len)
+console.log(upper)
+console.log(lower)
