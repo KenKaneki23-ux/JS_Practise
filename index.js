1fd3d290b4549ff7686deebbb81152a5de48173c
Ken <tushar260104@gmail.com>
2026-03-30 02:41:18 +0530

    Remove Space from string

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-function countDigit(str) {
-    let count = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] >= '0' && str[i] <= '9') {
-            count++;
-        }
-    }
-    return count;
-
+function removeSpaces(str) {
+    return str.replace(/\s/g, '');
 }
-console.log(countDigit('Tushar2023')); 
+console.log(removeSpaces('  Hello, World!  '));
