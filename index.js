247d8bc4fddf2787f7a12149d8d3f0bebab7d406
Ken <tushar260104@gmail.com>
2026-03-30 02:38:59 +0530

    COunt Digits in a sting

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-function countSpaces(str) {
+function countDigit(str) {
     let count = 0;
     for (let i = 0; i < str.length; i++) {
-        if (str[i] === ' ') {
+        if (str[i] >= '0' && str[i] <= '9') {
             count++;
         }
     }
     return count;
+
 }
-console.log(countSpaces('Tushar Das')); 
+console.log(countDigit('Tushar2023')); 
